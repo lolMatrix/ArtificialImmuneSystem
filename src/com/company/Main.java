9780ea4d2658d6c198b62156e5211ab93080333c
MatriX <dr.lesha@live.com>
2021-12-07 21:12:58 +0300

    [add] test functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,33 @@ import com.company.population.Population;
 import java.util.Arrays;
 import java.util.function.Function;
 
+import static java.lang.Math.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        Double[] fX = new Double[]{-10d};
         Function<Double[], Double> f = arr -> {
             double x = arr[0];
-            return Math.pow(x - 1, 2);
+            return pow(x - 1, 2);
         };
-        ArtificialImmuneSystem artificialImmuneSystem = new ArtificialImmuneSystem(f, new Double[]{1d},
-                100, -2d, 5d, 1, 3, 15);
-        artificialImmuneSystem.setBetta(100);
 
-        System.out.println(Arrays.toString(artificialImmuneSystem.start()));
+        Double[] sphereX = new Double[]{-100d, -100d};
+        Function<Double[], Double> sphere = arr -> {
+            double x = arr[0], y = arr[1];
+            return pow(x, 2) + pow(y, 2);
+        };
+
+        Double[] izomX = new Double[]{-100d, -100d};
+        Function<Double[], Double> izom = arr -> {
+            double x = arr[0], y = arr[1];
+            return -cos(x) * cos(y) * exp(-(pow(x - PI, 2) + pow(y - PI, 2)));
+        };
+
+        ArtificialImmuneSystem artificialImmuneSystem = new ArtificialImmuneSystem(f, fX,
+                1000, -100, 100, 2, 10, 15);
+        artificialImmuneSystem.setBetta(5);
+
+        System.out.println("xmin = " + Arrays.toString(artificialImmuneSystem.start()));
     }
 }
